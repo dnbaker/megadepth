8e2503afae5460742d3c1151221a79eda1e08298
ChristopherWilks <broadsword@gmail.com>
2020-07-29 20:35:01 -0400

    fixes that *now* allow non-curl windows binary to be made

--- build_no_container_windows.sh
+++ build_no_container_windows.sh
@@ -5,24 +5,39 @@ set -e
 build_type=$1
 bc=`perl -e '$bt="'$build_type'"; if($bt=~/static/i) { print "megadepth_static"; } elsif($bt=~/statlib/i) { print "megadepth_statlib"; } else { print "megadepth_dynamic"; }'`
 
-if [[ ! -s zlib ]] ; then
-    ./get_zlib.sh
+if [[ ! -s zlib-1.2.5-bin-x64 ]] ; then
+    wget "https://downloads.sourceforge.net/project/mingw-w64/External%20binary%20packages%20%28Win64%20hosted%29/Binaries%20%2864-bit%29/zlib-1.2.5-bin-x64.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fmingw-w64%2Ffiles%2FExternal%2520binary%2520packages%2520%2528Win64%2520hosted%2529%2FBinaries%2520%252864-bit%2529%2Fzlib-1.2.5-bin-x64.zip%2Fdownload&ts=1596067650" -O zlib-1.2.5-bin-x64.zip
+    mkdir zlib-1.2.5-bin-x64
+    pushd zlib-1.2.5-bin-x64
+    unzip ../zlib-1.2.5-bin-x64.zip
+    mv zlib/* ./
+    rm -rf zlib
+    popd
 fi
 
+if [[ ! -s curl-7.71.1-win64-mingw ]] ; then
+    wget "https://curl.haxx.se/windows/dl-7.71.1/curl-7.71.1-win64-mingw.zip" -O curl-7.71.1-win64-mingw.zip
+    unzip curl-7.71.1-win64-mingw.zip
+fi
+
+
 if [[ ! -s libdeflate ]] ; then
-    ./get_libdeflate.sh
+    ./get_libdeflate.sh windows
 fi
 
 if [[ ! -s htslib ]] ; then
-    export CPPFLAGS="-I../libdeflate -I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include"
-    export CFLAGS="-I../libdeflate -I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include"
-    export LDFLAGS="-L../libdeflate -L../zlib-1.2.5-bin-x64/lib -L../curl-7.71.1-win64-mingw/lib -ldeflate -lcurl"
+    export CFLAGS="-I../libdeflate -I../zlib-1.2.5-bin-x64/include"
+    export LDFLAGS="-L../libdeflate -L../zlib-1.2.5-bin-x64/lib -ldeflate"
+    
+    #export CPPFLAGS="-I../libdeflate -I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include"
+    #export CFLAGS="-I../libdeflate -I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include"
+    #export LDFLAGS="-L../libdeflate -L../zlib-1.2.5-bin-x64/lib -L../curl-7.71.1-win64-mingw/lib -ldeflate -lcurl"
     
     #export CPPFLAGS="-I../libdeflate_i686 -I../curl-7.71.1-win32-mingw/include -I../libz-1.2.7-1_mingw32-dev/include"
     #export CFLAGS="-I../libdeflate -I../curl-7.71.1-win32-mingw/include -I../libz-1.2.7-1_mingw32-dev/include"
     #export LDFLAGS="-L../libdeflate_i686 -L../libz-1.2.7-1_mingw32-dev/lib -L../curl-7.71.1-win32-mingw/lib -ldeflate -lcurl"
     
-    ./get_htslib.sh
+    ./get_htslib.sh windows
     export CPPFLAGS=
     export CFLAGS=
     export LDFLAGS=
@@ -35,7 +50,7 @@ fi
 set -x
 
 #compile a no-curl static version of libBigWig
-if [[ $bc == 'megadepth_static1' ]]; then
+if [[ $bc == 'megadepth_static' ]]; then
     pushd libBigWig
     #export CPPFLAGS="-I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include"
     #export CFLAGS="-I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include -g -Wall -O3 -Wsign-compare"
