72b5dac216cc2efe34ee1fcff0b9ef89c39cafeb
ChristopherWilks <broadsword@gmail.com>
2020-07-30 21:09:45 -0400

    streamlining cross compilation for windows/macos

--- build_no_container_windows.sh
+++ build_no_container_windows.sh
@@ -3,6 +3,13 @@
 set -e
 bc=megadepth_static
 
+#.e.g x86_64-w64-mingw32 or i686-w64-mingw32
+compiler=$1
+export CC=${compiler}-gcc
+export CXX=${compiler}-g++
+export AR=${compiler}-ar
+export RANLIB=${compiler}-ranlib
+
 if [[ ! -s mingw-std-threads ]]; then
     git clone https://github.com/meganz/mingw-std-threads.git
 fi
@@ -17,12 +24,18 @@ fi
 #    popd
 #fi
 
-if [[ ! -s curl-7.71.1-win64-mingw ]] ; then
+CURL_VER=7.71.1
+if [[ ! -s curl-${CURL_VER}-win64-mingw ]] ; then
     #https://curl.haxx.se/windows/dl-7.71.1/zlib-1.2.11-win64-mingw.zip
-    for f in curl-7.71.1-win64-mingw.zip libssh2-1.9.0-win64-mingw.zip openssl-1.1.1g-win64-mingw.zip nghttp2-1.41.0-win64-mingw.zip brotli-1.0.7-win64-mingw.zip zlib-1.2.11-win64-mingw.zip; do
-        wget  "https://curl.haxx.se/windows/${f}" -O $f
+    for f in curl-${CURL_VER}-win64-mingw.zip libssh2-1.9.0-win64-mingw.zip openssl-1.1.1g-win64-mingw.zip nghttp2-1.41.0-win64-mingw.zip brotli-1.0.7-win64-mingw.zip zlib-1.2.11-win64-mingw.zip; do
+        wget  "https://curl.haxx.se/windows/dl-${CURL_VER}/${f}" -O $f
         unzip $f
     done
+    ln -fs curl-${CURL_VER}-win64-mingw libcurl
+    ln -fs libssh2-1.9.0-win64-mingw libssh2
+    ln -fs openssl-1.1.1g-win64-mingw openssl
+    ln -fs nghttp2-1.41.0-win64-mingw nghttp2
+    ln -fs brotli-1.0.7-win64-mingw brotli
     ln -fs zlib-1.2.11-win64-mingw zlib
     #wget "https://curl.haxx.se/windows/dl-7.71.1/curl-7.71.1-win64-mingw.zip" -O curl-7.71.1-win64-mingw.zip
     #unzip curl-7.71.1-win64-mingw.zip
@@ -34,19 +47,13 @@ if [[ ! -s libdeflate ]] ; then
 fi
 
 if [[ ! -s htslib ]] ; then
-    export CFLAGS="-I../libdeflate -I../zlib-1.2.11-win64-mingw/include"
-    #export CFLAGS="-I../libdeflate -I../zlib-1.2.5-bin-x64/include"
-    #export LDFLAGS="-L../libdeflate -L../zlib-1.2.5-bin-x64/lib -ldeflate"
+    export CFLAGS="-I../libdeflate -I../zlib"
+    export LDFLAGS="-L../libdeflate -L../zlib -lz -ldeflate"
+    #export CFLAGS="-I../libdeflate -I../zlib-1.2.11-win64-mingw"
+    #export LDFLAGS="-L../libdeflate -L../zlib-1.2.11-win64-mingw -lz -ldeflate"
     
-    #export CPPFLAGS="-I../libdeflate -I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include"
-    #export CFLAGS="-I../libdeflate -I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include"
-    #export LDFLAGS="-L../libdeflate -L../zlib-1.2.5-bin-x64/lib -L../curl-7.71.1-win64-mingw/lib -ldeflate -lcurl"
-    
-    #export CPPFLAGS="-I../libdeflate_i686 -I../curl-7.71.1-win32-mingw/include -I../libz-1.2.7-1_mingw32-dev/include"
-    #export CFLAGS="-I../libdeflate -I../curl-7.71.1-win32-mingw/include -I../libz-1.2.7-1_mingw32-dev/include"
-    #export LDFLAGS="-L../libdeflate_i686 -L../libz-1.2.7-1_mingw32-dev/lib -L../curl-7.71.1-win32-mingw/lib -ldeflate -lcurl"
-    
-    ./get_htslib.sh windows
+    ./get_htslib.sh $compiler windows 
+
     export CPPFLAGS=
     export CFLAGS=
     export LDFLAGS=
@@ -55,16 +62,18 @@ fi
 if [[ ! -s libBigWig ]] ; then
     ./get_libBigWig.sh
     pushd libBigWig
-    export CFLAGS="-I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.11-win64-mingw/include -g -Wall -O3 -Wsign-compare -DCURL_STATICLIB"
+    export CFLAGS="-I../libcurl/include -I../zlib -g -Wall -O3 -Wsign-compare -DCURL_STATICLIB"
+    #export CFLAGS="-I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.11-win64-mingw -g -Wall -O3 -Wsign-compare -DCURL_STATICLIB"
     #export CFLAGS="-I../curl-7.71.1-win64-mingw/include -I../zlib-1.2.5-bin-x64/include -g -Wall -O3 -Wsign-compare -DCURL_STATICLIB"
     #export LDFLAGS="-L../zlib-1.2.5-bin-x64/lib -L../curl-7.71.1-win64-mingw/lib"
-    export AR=x86_64-w64-mingw32-ar
-    export RANDLIB=x86_64-w64-mingw32-ranlib
+    #export AR=x86_64-w64-mingw32-ar
+    #export RANDLIB=x86_64-w64-mingw32-ranlib
     #export CFLAGS="-I../zlib-1.2.5-bin-x64/include -g -Wall -O3 -Wsign-compare -DNOCURL"
     #export LDFLAGS="-L../zlib-1.2.5-bin-x64/lib -lz"
     make clean
+    make CC=${compiler}-gcc AR=$AR RANLIB=$RANLIB -f Makefile.fpic lib-static
     #make CC=x86_64-w64-mingw32-gcc -f Makefile.nocurl lib-static
-    make CC=x86_64-w64-mingw32-gcc -f Makefile.fpic lib-static
+    #make CC=x86_64-w64-mingw32-gcc -f Makefile.fpic lib-static
     export CPPFLAGS=
     export CFLAGS=
     export LDFLAGS=
@@ -76,7 +85,7 @@ set -x
 DR=build-release-temp
 mkdir -p ${DR}
 pushd ${DR}
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake CC=$CC CXX=$CXX AR=$AR RANLIB=$RANLIB -DCMAKE_BUILD_TYPE=Release ..
 make ${bc}
 popd
 cp ${DR}/${bc}.exe ./megadepth.exe
--- get_htslib.sh
+++ get_htslib.sh
@@ -2,13 +2,18 @@
 
 set -ex
 
-#e.g. i386-apple-darwin1 (CC=o32-gcc)
-#or x86_64-apple-darwin12-gcc (CC=o64-gcc)
-#CC must be set as well
-xcross=$1
+#macos:
+#   x86_64-apple-darwin12-gcc (CC=o64-gcc)
+#OR
+#   i386-apple-darwin1 (CC=o32-gcc)
+#windows:
+#   x86_64-w64-mingw32 (64bit)
+#OR
+#   i686-w64-mingw32 (32bit)
 
-#export CPPFLAGS="-I../libdeflate"
-#export LDFLAGS="-L../libdeflate -ldeflate"
+compiler=$1
+#"macos" or "windows" (or nothing for normal linux build)
+platform=$2
 
 #VER=1.9
 VER=1.10.2
@@ -27,33 +32,36 @@ autoheader
 autoconf
 make clean
 
-
-if [[ -z $xcross ]]; then
+if [[ -z $compiler ]]; then
     ./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate
     make
 else
+    ./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate --host=$compiler
     export CC=gcc
-    if [[ "$xcross" == "windows" ]]; then
-        #export CC=i686-w64-mingw32-gcc
-        export CC=x86_64-w64-mingw32-gcc
-        #./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate --host=i686-w64-mingw32
-        ./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate --host=x86_64-w64-mingw32
-        #mv configure configure.orig
-        #cat configure.orig | sed 's/cross_compiling=no/cross_compiling=yes/' > configure
-        #chmod a+x configure
-        #./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate
-        #make CC=i686-w64-mingw32-gcc libhts.a
-        make CC=x86_64-w64-mingw32-gcc libhts.a
-        #set cross_compiling=yes in htslib/configure to turn off mmap
-    else
-        ./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate --host=$xcross
+    if [[ "$xcross" == "macos" ]]; then
         #only make static lib for cross-compilation for now
-        export AR=/opt/osxcross/target/bin/x86_64-apple-darwin12-ar
-        export RANLIB=/opt/osxcross/target/bin/x86_64-apple-darwin12-ranlib
-        cp Makefile Makefile.orig
-        cat Makefile.orig | perl -ne 'chomp; $f=$_; $f=~s!^(AR\s*=\s*ar\s*)$!AR='$AR'!; $f=~s!^(RANLIB\s*=\s*ar\s*)$!AR='$RANLIB'!; print "$f\n";' > Makefile
-        make CC=$CC libhts.a
+        export CC=/opt/osxcross/target/bin/${compiler}-gcc
+        export AR=/opt/osxcross/target/bin/${compiler}-ar
+        export RANLIB=/opt/osxcross/target/bin/${compiler}-ranlib
+        #export AR=/opt/osxcross/target/bin/x86_64-apple-darwin12-ar
+        #export RANLIB=/opt/osxcross/target/bin/x86_64-apple-darwin12-ranlib
+        #cp Makefile Makefile.orig
+        #cat Makefile.orig | perl -ne 'chomp; $f=$_; $f=~s!^(AR\s*=\s*ar\s*)$!AR='$AR'!; $f=~s!^(RANLIB\s*=\s*ar\s*)$!AR='$RANLIB'!; print "$f\n";' > Makefile
+        #make CC=$CC libhts.a
+    else # windows
+        #export CC=i686-w64-mingw32-gcc
+        #export CC=x86_64-w64-mingw32-gcc
+        export CC=${compiler}-gcc
+        export AR=${compiler}-ar
+        export RANLIB=${compiler}-ranlib
+        #make libhts.a
+        #./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate --host=i686-w64-mingw32
+        #./configure --disable-bz2 --disable-lzma --disable-libcurl --with-libdeflate --host=x86_64-w64-mingw32
+        #make CC=i686-w64-mingw32-gcc libhts.a
+        #make CC=x86_64-w64-mingw32-gcc libhts.a
+        #set cross_compiling=yes in htslib/configure to turn off mmap
     fi
+    make CC=$CC AR=$AR RANLIB=$RANLIB libhts.a
 fi
 
 popd
